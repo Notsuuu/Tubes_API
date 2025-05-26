e2acf9c8c8ebfc5c8d3a2778ac367d740bb8ae22
Fiorentino24 <154525917+Fiorentino24@users.noreply.github.com>
2025-05-26 09:45:14 +0800

    Penggantian Model openai.js

--- backend/utils/openai.js
+++ backend/utils/openai.js
@@ -5,7 +5,7 @@ const askOpenAI = async (prompt) => {
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
-        model: "gpt-3.5-turbo",
+        model: "gpt-4o",
         messages: [
           {
             role: "system",
